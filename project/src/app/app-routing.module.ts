312dc30e7d4e669f2ce47a268fa9474148a76f69
Celine <celi.caurier@gmail.com>
2020-06-12 19:07:15 +0200

    Link Component to app

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthComponent } from './auth/auth.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FollowComponent } from './follow/follow.component';
+import { MeetingComponent } from './meeting/meeting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component : AuthComponent },
+  { path:'profile', component : ProfileComponent },
+  { path:'meeting', component : MeetingComponent },
+  { path:'follow', component : FollowComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule} from './material.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthComponent } from './auth/auth.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FollowComponent } from './follow/follow.component';
+import { MeetingComponent } from './meeting/meeting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    ProfileComponent,
+    FollowComponent,
+    MeetingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
