0c21e5c3596fa5f18ec416b55220ac0b8953fbbc
Celine <celi.caurier@gmail.com>
2020-06-15 20:37:17 +0200

    authentification

--- project/src/app/auth/auth.component.ts
+++ project/src/app/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+  
+  AuthForm : FormGroup;
+  authstatus = false;
+  hide = true;
+  error = false;
 
-  constructor() { }
+  constructor( private router: Router , private authService: AuthService, private formBuilder : FormBuilder) { }
 
-  ngOnInit(): void {
+  initForm(){
+    this.AuthForm = this.formBuilder.group({
+      identifiant: '',
+      mdp:'',
+    })
   }
 
+  ngOnInit(): void {
+    this.initForm();
+    this.authstatus = this.authService.isAuth
+    this.error = this.authService.error;
+  }
+
+  onsignIn(){
+    const formValue = this.AuthForm.value;
+    this.authService.signIn(formValue)
+    // console.log(formValue)
+    this.error = this.authService.error;
+    this.authstatus = this.authService.isAuth;
+    this.router.navigate(['profile']);
+      }
+    
+
 }
+
+
