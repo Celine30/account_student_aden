95ca1c85c032e3ab67468f8ad563a7ea4bda0e40
Celine <celi.caurier@gmail.com>
2020-06-18 09:34:38 +0200

    Membership form

--- project/src/app/new-profil/new-profil.component.ts
+++ project/src/app/new-profil/new-profil.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-new-profil',
@@ -12,6 +12,16 @@ export class NewProfilComponent implements OnInit {
   secondFormGroup: FormGroup;
   threeFormGroup: FormGroup;
   forFormGroup: FormGroup;
+
+  formGroup: FormGroup;
+  
+  identityFormGroup: FormGroup;
+  // inforFormGroup: FormGroup;
+  // formaFormGroup: FormGroup;
+  // creatFormGroup: FormGroup;
+
+  /** Returns a FormArray with the name 'formArray'. */
+  get formArray(): AbstractControl | null { return this.formGroup.get('formArray'); }
 
   constructor(private _formBuilder: FormBuilder) { }
 
@@ -51,9 +61,85 @@ export class NewProfilComponent implements OnInit {
     // this.secondFormGroup = this._formBuilder.group({
     //   cotrol: ['', Validators.required],
     // });
-  }
+    this.formGroup = this._formBuilder.group({
+      formArray: this._formBuilder.array([
+        this._formBuilder.group({
+          name: ['', Validators.required],
+          firstname: ['', Validators.required],
+          email: ['', Validators.required],
+          phone1: ['', Validators.required],
+          phone2: ['', Validators.required],
+          adress: ['', Validators.required],
+          postal: ['', Validators.required],
+          city: ['', Validators.required],
+          department: ['', Validators.required],
+          region: ['', Validators.required]
+        }),
+        this._formBuilder.group({
+          birthday: ['', Validators.required],
+          citybirthday: ['', Validators.required],
+          countrybirthday: ['', Validators.required],
+          nationality: ['', Validators.required],
+          statut: ['', Validators.required],
+          numberSecu: ['', Validators.required],
+          licence: ['', Validators.required],
+          vehicule: ['', Validators.required],
+          mobile: ['', Validators.required],
+          handicapped: ['', Validators.required]
+        }),
+        this._formBuilder.group({
+          training: ['', Validators.required],
+          funding: ['', Validators.required]
+        }),
+        this._formBuilder.group({
+          password: ['', Validators.required]
+        }),
+      ])
+    });
 
+    // this.identityFormGroup = this._formBuilder.group({
+    //   name: ['', Validators.required],
+    //   firstname: ['', Validators.required],
+    //   email: ['', Validators.required],
+    //   phone1: ['', Validators.required],
+    //   phone2: ['', Validators.required],
+    //   adress: ['', Validators.required],
+    //   postal: ['', Validators.required],
+    //   city: ['', Validators.required],
+    //   department: ['', Validators.required],
+    //   region: ['', Validators.required]
+    // });
+    // this.inforFormGroup= this._formBuilder.group({
+    //   birthday: ['', Validators.required],
+    //   citybirthday: ['', Validators.required],
+    //   countrybirthday: ['', Validators.required],
+    //   nationality: ['', Validators.required],
+    //   statut: ['', Validators.required],
+    //   numberSecu: ['', Validators.required],
+    //   licence: ['', Validators.required],
+    //   vehicule: ['', Validators.required],
+    //   mobile: ['', Validators.required],
+    //   handicapped: ['', Validators.required]
+    // });
+    // this.formaFormGroup= this._formBuilder.group({
+    //   training: ['', Validators.required],
+    //   funding: ['', Validators.required]
+    // });
+    // this.creatFormGroup= this._formBuilder.group({
+    //   password: ['', Validators.required]
+    // });
+
+
+  }
+  form1(){
+    console.log(this.firstFormGroup.value);
+  }
+  form2(){
+    console.log(this.secondFormGroup.value);
+  }
+  
   onSubmit(){
-    console.log('envoyer')
+    console.log('envoyer');
+    console.log(this.formGroup.value)
   }
 }
