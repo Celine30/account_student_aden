cfdb893020ea4a0e4e6fde468a216cc9edf8e4d4
Celine <celi.caurier@gmail.com>
2020-08-17 22:59:38 +0200

    Work UnitFolow

--- project/src/app/services/formation.service.ts
+++ project/src/app/services/formation.service.ts
@@ -10,12 +10,20 @@ export class FormationService{
 
     private formationsSuivies = []
 
+    private formationOneSuivie = []
+
     public informations:Object
 
     formationsSuiviesSubject = new Subject<any>();
 
+    formationOneSuivieSubject = new Subject<any>();
+
     emitFormationsSuiviesSubject (){
         this.formationsSuiviesSubject.next(this.formationsSuivies.slice());
+    }
+
+    emitFormationOneSuivieSubject (){
+        this.formationOneSuivieSubject.next(this.formationOneSuivie.slice());
     }
 
     getFormationsSuiviesToServer($ID_connected){
@@ -26,10 +34,11 @@ export class FormationService{
                 (responses) =>{
                     responses.forEach(
                         element =>this.formationsSuivies.push(
-                            {position: element.position, entreprise: element.entreprise, contact: element.name_contact, outils : element.contact, response:element.response, relaunch : element.relauchdate, ID_entreprise : element.ID }
+                            {position: element.position, entreprise: element.entreprise, date_added: element.date_added, contact: element.name_contact, outils : element.contact, response:element.response, relaunch : element.relauchdate, ID_entreprise : element.ID }
                         )
                     )
                     this.formationsSuivies=this.formationsSuivies.reverse();
+                    
                     this.emitFormationsSuiviesSubject()
                 },
                 (error) => {
@@ -46,7 +55,8 @@ export class FormationService{
             .post<any[]>('http://localhost:8888/API-aden/index.php?action=back!edit_contactEntreprise', JSON.stringify(this.informations))
             .subscribe(
                 (response) =>{
-                    console.log(response)
+                    this.formationOneSuivie=response;
+                    this.emitFormationOneSuivieSubject()
                 },
                 (error) => {
                     console.log('Erreur de chargement' + error);
--- project/src/app/unit-follow/unit-follow.component.ts
+++ project/src/app/unit-follow/unit-follow.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormationService } from  '../services/formation.service'
+import { Subscription } from 'rxjs';
+import { EntrepriseUnit } from '../entrepriseUnit'
 
 
 @Component({
@@ -11,13 +13,23 @@ import { FormationService } from  '../services/formation.service'
 export class UnitFollowComponent implements OnInit {
 
   IdPost
+ 
+  formationOneSuivieSubscription : Subscription;
+  informationsEntreprise:object;
 
   constructor(private router :Router, private route: ActivatedRoute , private FormationService : FormationService) { }
 
   ngOnInit(): void {
     this.IdPost=this.route.snapshot.params['id']
     console.log(this.IdPost)
-    this.FormationService.getAllContact(sessionStorage.getItem('ID_connected'),this.IdPost)
+    this.FormationService.getAllContact(sessionStorage.getItem('ID_connected'),this.IdPost);
+    this.formationOneSuivieSubscription = this.FormationService.formationOneSuivieSubject.subscribe(
+      (response)=> {
+        console.log(response)
+        this.informationsEntreprise = new EntrepriseUnit(response[0][0].entreprise,response[0][0].date_added,response[0][0].name_contact, response[0][0].contact,response[0][0].response,response[1])
+        console.log(this.informationsEntreprise)
+      }
+    )
   }
 
 }
