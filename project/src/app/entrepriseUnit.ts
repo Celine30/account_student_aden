031c9505e5d8064b5d1a0c94b304517e37545296
Celine <celi.caurier@gmail.com>
2020-08-17 22:56:32 +0200

    Create EntrepriseUnit

---
+++ project/src/app/entrepriseUnit.ts
@@ -0,0 +1,11 @@
+export class EntrepriseUnit {
+
+    constructor (
+            public entreprise: string,
+            public date_added: string,
+            public name_contact: string,
+            public outils:any,
+            public response: string,
+            public contact: any){}
+     
+}
